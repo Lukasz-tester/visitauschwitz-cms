3ea926db331bf65f941bc6dd73b4f3a3f11ef45d
Lukasz-tester <wladomania@wp.pl>
2025-01-03 09:43:21 +0100

    old loge removed

--- src/components/LivePreviewListener/index.tsx
+++ src/components/LivePreviewListener/index.tsx
@@ -1,6 +1,7 @@
 'use client'
-import { RefreshRouteOnSave as PayloadLivePreview } from '@payloadcms/live-preview-react'
 import { useRouter } from 'next/navigation'
+import { RefreshRouteOnSave as PayloadLivePreview } from '@payloadcms/live-preview-react'
+
 import React from 'react'
 
 export const LivePreviewListener: React.FC = () => {
