c0ba53b7c1a0ff0949f554adea892cebe5c8888b
Lukasz-tester <wladomania@wp.pl>
2025-05-12 23:23:30 +0200

    middleware lol

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,14 +26,14 @@ export default function middleware(request: NextRequest) {
   return response
 }
 
-// export const config = {
-//   matcher: [
-//     // Match all pathnames except for
-//     // - … if they start with `/api`, `/_next`, `/_vercel`, or `/admin`
-//     // - … the ones containing a dot (e.g. `favicon.ico`)
-//     '/((?!api|_next|_vercel|admin|next|.*\\..*).*)',
-//   ],
-// }
+export const config = {
+  matcher: [
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/_next`, `/_vercel`, or `/admin`
+    // - … the ones containing a dot (e.g. `favicon.ico`)
+    '/((?!api|_next|_vercel|admin|next|.*\\..*).*)',
+  ],
+}
 
 // BELOW comment is not letting home be cached which is good for bfcache raw json response, but not good for performance
 //the code was between declaring and returning response
